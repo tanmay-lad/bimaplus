9290c9cd5340efda3f5c8cb2b9b76737bd1c4ba3
tanmay-lad <tanmay.lad00@gmail.com>
2024-05-03 14:35:51 +0530

    modified:   chatbot.py 	modified:   requirements.txt

--- chatbot.py
+++ chatbot.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import chromadb
 import chromadb.utils.embedding_functions as embedding_functions
 import pandas as pd
